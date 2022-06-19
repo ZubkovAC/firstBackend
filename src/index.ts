85974cc3ea52148082f714403206b09a5a93acfd
Aleksandr <zybkob@f-case.ru>
2022-06-20 01:57:33 +0300

    fix lesson_01

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ app.use(cors())
 app.use(express.json())
 // app.use(express.urlencoded({extended: true}))
 
+
 app.get('/', (req, res) => {
     let helloWorldWORLD11 = 'Hello World! WORLD!';
     res.send(helloWorldWORLD11)
