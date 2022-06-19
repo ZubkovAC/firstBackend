f689bad43222c426e6ca60f2e09dafd041b5893a
Aleksandr <zybkob@f-case.ru>
2022-06-19 14:39:35 +0300

    fix lesson_01 post

--- src/routes/lesson_01.ts
+++ src/routes/lesson_01.ts
@@ -2,90 +2,28 @@ import {Request, Response, Router} from "express";
 
 export const lesson_01_Router = Router({})
 
-    const videosLesson01 = [
-        {
+const videosLesson01 = [
+    {
         "id": 0,
         "title": "The Green Mile",
         "author": "Frank Darabont"
-        },
-        {
+    },
+    {
         "id": 1,
         "title": "Green Book",
         "author": "Peter Farrelly"
-        },
-    ]
+    },
+]
 
-lesson_01_Router.get('/api/videos',(req: Request, res: Response) => {
+lesson_01_Router.get('/api/videos', (req: Request, res: Response) => {
     res.status(201).send(videosLesson01)
 })
 
-lesson_01_Router.post('/api/videos',(req: Request, res: Response) => {
+lesson_01_Router.post('/api/videos', (req: Request, res: Response) => {
 
-    if(req.body.title === null){
-        res.status(400).send({
-            "errorsMessages": [
-                {
-                    "message": "string",
-                    "field": "string"
-                }
-            ]
-        })
-        return;
-    }
+    const title = req.body.title
 
-    if( req.body.title.trim().length !== 0 || req.body.title.length <= 40 || typeof req.body.title === 'string' || req.body.title !== null){
-        const newVideo = {
-            "id": Math.floor(Math.random()*10000) ,
-            "title":req.body.title.trim(),
-            "author": "Peter Farrelly-moc"
-        }
-        videosLesson01.push(newVideo)
-        res.status(200).send(newVideo)
-        return
-    }
-    res.status(400).send({
-        "errorsMessages": [
-            {
-                "message": "string",
-                "field": "string"
-            }
-        ]
-    })
-})
-lesson_01_Router.get('/api/videos/:id',(req: Request, res: Response) => {
-    const id = +req.params.id
-    const videoId = videosLesson01.find(v=>v.id === id)
-    if(videoId){
-        res.status(204).send(videoId)
-        return
-    }
-    res.status(404).send("If video for passed id doesn't exist")
-})
-
-lesson_01_Router.put('/api/videos/:id',(req: Request, res: Response) => {
-
-    if(req.body.title === null){
-        res.status(400).send({
-            "errorsMessages": [
-                {
-                    "message": "string",
-                    "field": "string"
-                }
-            ]
-        })
-        return;
-    }
-
-
-    const id = +req.params.id
-    const newTitle = req.body.title
-    const videoId = videosLesson01.find(v=>v.id === id)
-    if(videoId && newTitle.length <= 40 || req.body.title.trim().length !== 0 || typeof req.body.title === 'string' || req.body.title !== null){
-        videoId.title = newTitle
-        res.status(200).send(videoId)
-        return
-    }
-    if(videoId && newTitle.length > 40 ){
+    if (!title || typeof req.body.title === 'string' || title.trim() || title.length > 40) {
         res.status(400).send({
             "errorsMessages": [
                 {
@@ -93,30 +31,61 @@ lesson_01_Router.put('/api/videos/:id',(req: Request, res: Response) => {
                     "field": "string"
                 }
             ],
-            "resultCode": 0
+            resultCode: 1
         })
-        return
-    }
-    if(!videoId){
-        res.status(204).send('No Content')
         return;
     }
-    res.status(404).send({
-        "errorsMessages": [
-            {
-                "message": "string",
-                "field": "string"
-            }
-        ],
-        "resultCode": 0
-    })
-    return;
+
+    const newVideo = {
+        "id": Math.floor(Math.random() * 10000),
+        "title": req.body.title.trim(),
+        "author": "Peter Farrelly-moc"
+    }
+    videosLesson01.push(newVideo)
+    res.status(200).send(newVideo)
+    return
 })
 
-lesson_01_Router.delete('/api/videos/:id',(req: Request, res: Response) => {
+lesson_01_Router.get('/api/videos/:id', (req: Request, res: Response) => {
+    const id = +req.params.id
+    const videoId = videosLesson01.find(v => v.id === id)
+    if (videoId) {
+        res.status(204).send(videoId)
+        return
+    }
+    res.status(404).send("If video for passed id doesn't exist")
+})
+
+lesson_01_Router.put('/api/videos/:id', (req: Request, res: Response) => {
+
+    const title = req.body.title
+
+    if (!title || typeof req.body.title === 'string' || title.trim() || title.length > 40) {
+        res.status(400).send({
+            "errorsMessages": [
+                {
+                    "message": "string",
+                    "field": "string"
+                }
+            ],
+            resultCode: 1
+        })
+        return;
+    }
+
+    const id = +req.params.id
+    const videoId = videosLesson01.find(v => v.id === id)
+    if (videoId) {
+        res.send(204)
+    }else{
+        res.send(404)
+    }
+})
+
+lesson_01_Router.delete('/api/videos/:id', (req: Request, res: Response) => {
     const videoDeleteId = +req.params.id
-    const newVideo = videosLesson01.filter(v=>v.id !== videoDeleteId)
-    if(newVideo.length === videosLesson01.length){
+    const newVideo = videosLesson01.filter(v => v.id !== videoDeleteId)
+    if (newVideo.length === videosLesson01.length) {
         res.status(204)
         return
     }
