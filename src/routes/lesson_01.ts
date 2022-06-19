7fb4a1802b0cfcc9fab58c542f3a16b16271c3b1
Aleksandr <zybkob@f-case.ru>
2022-06-19 13:31:55 +0300

    fix lesson_01 post

--- src/routes/lesson_01.ts
+++ src/routes/lesson_01.ts
@@ -20,7 +20,7 @@ lesson_01_Router.get('/api/videos',(req: Request, res: Response) => {
 })
 
 lesson_01_Router.post('/api/videos',(req: Request, res: Response) => {
-    if(!req.body.title.trim() || req.body.title.length <= 40 || typeof req.body.title !== 'string'){
+    if(!req.body.title.trim() || req.body.title.length <= 40 || typeof req.body.title === 'string'){
         const newVideo = {
             "id": Math.floor(Math.random()*10000) ,
             "title":req.body.title,
@@ -53,7 +53,7 @@ lesson_01_Router.put('/api/videos/:id',(req: Request, res: Response) => {
     const id = +req.params.id
     const newTitle = req.body.title
     const videoId = videosLesson01.find(v=>v.id === id)
-    if(videoId && newTitle.length <= 40 || !req.body.title.trim() || typeof req.body.title !== 'string'){
+    if(videoId && newTitle.length <= 40 || !req.body.title.trim() || typeof req.body.title === 'string'){
         videoId.title = newTitle
         res.status(200).send(videoId)
         return
