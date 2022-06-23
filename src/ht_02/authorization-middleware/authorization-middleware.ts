b8e16660e58f0d36e65a90b3a50aa0e241f05f79
Aleksandr <zybkob@f-case.ru>
2022-06-23 14:13:46 +0300

    ht_02 (fix auth)

--- src/ht_02/authorization-middleware/authorization-middleware.ts
+++ src/ht_02/authorization-middleware/authorization-middleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction,Response,Request} from "express";
 
 export const authorizationMiddleware = (req: Request, res: Response ,next: NextFunction) => {
-    let authorization = req.headers.authorization.split(' ')[1]
+    let authorization = req.headers?.authorization?.split(' ')[1]
     if(authorization === 'YWRtaW46cXdlcnR5'){
         next()
         return
