77250431a1c2eb3a71138f72663f2075fbbdb878
Aleksandr <zybkob@f-case.ru>
2022-07-17 20:14:53 +0300

    mongo server

--- src/ht_03/db.ts
+++ src/ht_03/db.ts
@@ -14,19 +14,23 @@ export type PostsType = {
     "bloggerName": string
 }
 
-const mongoUri = process.env.mongoURI || 'mongodb://0.0.0.0:27017'
+// const mongoUri = process.env.mongoURI || 'mongodb://0.0.0.0:27017'
+const mongoUri = "mongodb+srv://ZybkovAC:KalxotXic2bEZh4R@cluster0.ywntv.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://0.0.0.0:27017'
 
 export const client = new MongoClient(mongoUri)
 
-let db = client.db('video');
+// let db = client.db('video');
+let db = client.db('expample');
 export const bloggersCollection = db.collection<BloggersType>('bloggers')
 export const postsCollection = db.collection<PostsType>('posts')
 
 export async function runDb(){
     try{
         await client.connect()
-        await client.db('video').command({ping:1})
+        // await client.db('videos').command({ping:1})
+        await client.db('expample').command({ping:1})
         console.log('Connect successfully to Mongo server')
+        console.log('соедениние установлено')
     }catch {
         console.log('Can"t connect to db')
         await client.close()
