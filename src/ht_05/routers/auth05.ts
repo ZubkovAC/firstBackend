7bc437701439c8fb48715a8d1c117bf3e94f92f1
Aleksandr <zybkob@f-case.ru>
2022-08-06 02:16:01 +0300

    05 - gmail

--- src/ht_05/routers/auth05.ts
+++ src/ht_05/routers/auth05.ts
@@ -1,7 +1,8 @@
 import {Request, Response, Router} from "express";
 import {validationErrorAuth, validationLogin3_10, validationPassword6_20} from "../../validation/validation";
 import {secret, usersCollection} from "../db";
-import nodemailer from 'nodemailer'
+const nodemailer = require("nodemailer")
+
 var jwt = require('jsonwebtoken')
 
 export const RouterAuth05 = Router({})
@@ -10,36 +11,41 @@ RouterAuth05.post("/registration-confirmation",
     async (req, res) => {
         res.send(`registration-confirmation + ${process.env.EMAIL} + ${process.env.PASSWORD}`)
         return
-})
+    })
 
 RouterAuth05.post("/registration",
     async (req, res) => {
-    const login = req.body.login
-    const password = req.body.password
-    const email = req.body.email
+        const login = req.body.login
+        const password = req.body.password
+        const email = req.body.email
+
         let transporter = nodemailer.createTransport({
-            service:"gmail",
-            // secure: false, // true for 465, false for other ports
+            service: "gmail",
             auth: {
-                user: process.env.EMAIL, // generated ethereal user
-                pass:  process.env.PASSWORD, // generated ethereal password
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD,
             },
         });
         let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
-            to: `${process.env.EMAIL}`, // list of receivers
+            from: '1233312', // sender address
+            to: '3y6kob90@gmail.com', // list of receivers
             subject: "Registration ✔", // Subject line
             text: "Access Email", // plain text body
             html: "<b>Hello world?</b>", // html body
         });
-            res.send('registration')
-            return
-})
+        console.log('info-registration',info)
+        res.send({
+            email: email,
+            login: login,
+            password: password
+        })
+        return
+    })
 RouterAuth05.post("/registration-email-resending",
     async (req, res) => {
-            res.send('registration-email-resending')
-            return
-})
+        res.send('registration-email-resending')
+        return
+    })
 RouterAuth05.post('/login',
     validationLogin3_10,
     validationPassword6_20,
@@ -48,12 +54,12 @@ RouterAuth05.post('/login',
         // const parse = jwt.verify(test,secret.key)
         const login = req.body.login.trim()
         const password = req.body.password.trim()
-        const searchLogin = await usersCollection.findOne({login:login})
-        if(searchLogin && searchLogin.password === password){
-            const token = jwt.sign({id:searchLogin.id},secret.key,{expiresIn:'1h'})
-            res.status(200).send({token:token})
+        const searchLogin = await usersCollection.findOne({login: login})
+        if (searchLogin && searchLogin.password === password) {
+            const token = jwt.sign({id: searchLogin.id}, secret.key, {expiresIn: '1h'})
+            res.status(200).send({token: token})
             return
         }
         res.status(401).send('If the password or login is wrong')
         return
-})
+    })
