c4b8327b9eb7371976e21dc4ac3e4c0bb34827e8
Aleksandr <zybkob@f-case.ru>
2022-08-10 22:56:11 +0300

    05 - registration ?

--- src/ht_05/repositories/count-repositories05.ts
+++ src/ht_05/repositories/count-repositories05.ts
@@ -34,7 +34,7 @@ export const CountRepositories05 ={
         const date = new Date()
         const repo = searchRepo(path)
         const req = await repo.find({ip:ip}).toArray()
-        const f = req.filter(d=> addSeconds(d.date,9) > date  )
+        const f = req.filter(d=> addSeconds(d.date,11) > date  )
         console.log('test f',f.length)
         return f?.length >= 5
     },
