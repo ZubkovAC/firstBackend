527ff1e84f27328736aeb87e5f4738a7ef65a81d
Aleksandr <zybkob@f-case.ru>
2022-08-10 12:52:44 +0300

    05 - fix message email

--- src/ht_05/adapter/emailAdapter.ts
+++ src/ht_05/adapter/emailAdapter.ts
@@ -14,9 +14,9 @@ export const EmailAdapter05 = {
             from: `3y6kob <${emailFrom}>`, // sender address
             to: emailTo, // list of receivers
             subject: "Registration ✔", // Subject line
-            text: "Access Email", // plain text body
+            text: `https://some-front.com/confirm-registration?code=${code}`, // plain text body
             // html: `<h1>confirmation email</h1><div><a href=https://ferst-back.herokuapp.com/ht_05/api/auth/confirm-registration?code=${code}>click</a></div> `, // html body
-            html: `https://ferst-back.herokuapp.com/ht_05/api/auth/confirm-registration?code=${code}`, // html body
+            // html: `<div><a href=https://some-front.com/confirm-registration?code=${code}>https://some-front.com/confirm-registration?code=${code}</a></div>`, // html body
             // html: "https://ferst-back.herokuapp.com/ht_05/api/auth/confirm-registration?code=youtcodehere", // html body
         }
     },
