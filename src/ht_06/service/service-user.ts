987a7dfe576597a354cb003614519def02592461
Aleksandr <zybkob@f-case.ru>
2022-08-14 14:30:30 +0300

    06 - test

--- src/ht_06/service/service-user.ts
+++ src/ht_06/service/service-user.ts
@@ -8,7 +8,7 @@ export const serviceUser04 ={
        return  await usersRepositories06.getUsers(pageNumber,pageSize)
     },
     async createUsers(login:string, password:string,email:string){
-       return  await usersRepositories06.createUser(login,password)
+       return  await usersRepositories06.createUser(login,password,email)
     },
     async deleteUsers(userId:string){
        return  await usersRepositories06.deleteUser(userId)
