cef084ea2d8f589e891213b9ecfb95caab7b5368
Aleksandr <zybkob@f-case.ru>
2022-08-16 13:21:43 +0300

    06 - fix me - token

--- src/ht_06/routers/auth06.ts
+++ src/ht_06/routers/auth06.ts
@@ -208,7 +208,7 @@ RouterAuth06.post('/logout',
        }
     })
 RouterAuth06.get('/me',
-    authorizationMiddleware06,
+    // authorizationMiddleware06,
     async (req: Request, res: Response) => {
         const token = req.headers?.authorization
         if(token){
